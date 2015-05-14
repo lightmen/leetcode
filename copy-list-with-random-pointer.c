8c27b66f63974aa1006e9f739bda5d0f13b8b6fc
lightmen <519542738@qq.com>
2015-05-14 22:06:04 +0800

    add copy-list-with-random-pointer.c

---
+++ copy-list-with-random-pointer.c
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list with a random pointer.
+ * struct RandomListNode {
+ *     int label;
+ *     struct RandomListNode *next;
+ *     struct RandomListNode *random;
+ * };
+ */
+struct RandomListNode *copyRandomList(struct RandomListNode *head) {
+    struct RandomListNode root;
+    struct RandomListNode *prev,*tmp,*p,*tmp_head;
+    struct RandomListNode **arr;
+    int count = 0;
+    int i;
+    if(!head)
+        return NULL;
+
+    root.next = NULL;
+    prev = &root;
+    while(head){
+        tmp = (struct RandomListNode *)malloc(sizeof(struct RandomListNode));
+        tmp->label = head->label;
+        tmp->next = NULL;
+        prev->next = tmp;
+
+        p = head->next;
+        head->next = tmp;
+        tmp->random = head;
+        
+        prev = tmp;
+        head = p;
+        count++;
+    }
+    
+    arr = (struct RandomListNode **)malloc(sizeof(struct RandomListNode *) * count);
+    i = 0;
+    head = root.next;
+    while(head){
+        p = head->random;
+        tmp = p->random;
+        arr[i++] = p;
+        head->random = tmp ? tmp->next : NULL;
+        head = head->next;
+    }
+    
+    for(i = 0; i < count-1; ++i)
+        arr[i]->next = arr[i+1];
+    arr[count-1]->next = NULL;
+    head = arr[0];
+    return root.next;
+}

6b32295b87222edeb575bf792f6c9768fc93ef5c
lightmen <519542738@qq.com>
2015-05-30 22:07:11 +0800

    add house-robber-ii.c

---
+++ house-robber-ii.c
@@ -0,0 +1,28 @@
+int rob(int* nums, int numsSize) {
+    if(numsSize == 1)
+        return nums[0];
+    int *dp0,*dp1;
+    int i,ret;
+    dp0 = (int *)malloc(sizeof(int) * (numsSize + 1));
+    dp1 = (int *)malloc(sizeof(int) * (numsSize + 1));
+    
+    dp0[0] = 0;
+    dp0[1] = nums[0];
+    for(i = 2; i < numsSize; ++i)
+        dp0[i] = (dp0[i-1] > dp0[i-2] + nums[i-1] ? 
+            dp0[i-1] : dp0[i-2] + nums[i-1]);
+    dp0[i] = dp0[i-1] > dp0[i-2] ? dp0[i-1] : dp0[i-2];
+    
+    dp1[0] = 0;
+    dp1[1] = 0;
+    for(i = 2; i <= numsSize; ++i)
+        dp1[i] = (dp1[i-1] > dp1[i-2] + nums[i-1] ? 
+                dp1[i-1] : dp1[i-2] + nums[i-1]);
+                
+    ret = dp0[numsSize] > dp1[numsSize] ? 
+            dp0[numsSize] : dp1[numsSize];
+    free(dp0);
+    free(dp1);
+    
+    return ret;
+}

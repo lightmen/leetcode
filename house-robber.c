67ca231bb63ac936b7c71a994b53a7b3a133c9a0
lightmen <519542738@qq.com>
2015-05-30 22:09:41 +0800

    modify house-robber.c

--- house-robber.c
+++ house-robber.c
@@ -1,11 +1,14 @@
 int rob(int num[], int n) {
     int *dp;
-    int i;
+    int i,ret;
     dp = (int *)malloc(sizeof(int) * (n+1));
     dp[0] = 0;
     dp[1] = num[0];
     for(i = 2; i <= n; ++i)
         dp[i] = (dp[i-1] > (dp[i-2] + num[i-1])) ?
             dp[i-1] : dp[i-2] + num[i-1];
-    return dp[n];
+    ret = dp[n];
+
+    free(dp);
+    return ret;
 }

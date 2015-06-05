0d83feacdf7965723f6877ece0d621cf6de2d986
lightmen <519542738@qq.com>
2015-06-05 22:30:49 +0800

    add maximal-square.c

---
+++ maximal-square.c
@@ -0,0 +1,38 @@
+int maximalSquare(char** matrix, int row, int col) {
+    int **dp;
+    int i,j;
+    int ret,min;
+    ret = 0;
+    if(row == 0 || col == 0)
+        return 0;
+    dp = (int **)malloc(sizeof(int *) * row);
+    for(i = 0; i < row; ++i){
+        dp[i] = (int *)malloc(sizeof(int) * col);
+        memset(dp[i],0,sizeof(int) * col);
+    }
+
+    for(i = 0; i < row; ++i){
+        for(j = 0; j < col; ++j){
+            if(i-1 < 0 || j-1 < 0 || matrix[i][j] == '0')
+                min = 0;
+            else{
+                min = dp[i-1][j] < dp[i][j-1] ? dp[i-1][j] : dp[i][j-1];
+                min = dp[i-1][j-1] < min ? dp[i-1][j-1] : min;
+            }
+
+            if(matrix[i][j] == '1')
+                min++;
+
+            dp[i][j] = min;
+
+            if(min * min > ret)
+                ret = min * min;
+        }
+    }
+
+    for(i = 0; i < row; ++i)
+        free(dp[i]);
+    free(dp);
+
+    return ret;
+}

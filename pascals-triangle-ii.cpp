b6803c56811e070bf452fd5cb86270df375d29b5
lightmen <519542738@qq.com>
2015-06-07 21:40:27 +0800

    add pascals-triangle-ii.cpp

---
+++ pascals-triangle-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        int i = 0,cnt = 0,size;
+        vector<vector<int> > vret;
+        vector<int> vi,prev;
+
+        vi.push_back(1);
+        vret.push_back(vi);
+
+        while(cnt < rowIndex){
+            prev = vret[cnt++];
+            size = prev.size();
+            vi.clear();
+            vi.push_back(1);
+            for(i = 0; i < size-1; ++i)
+                vi.push_back(prev[i] + prev[i+1]);
+            vi.push_back(1);
+
+            vret.push_back(vi);
+        }
+
+        vector<int> ret(vret[rowIndex].begin(),vret[rowIndex].end());
+        return ret;
+    }
+};

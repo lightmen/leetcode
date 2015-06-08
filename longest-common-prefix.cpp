bf343008f2f75996f61f3bf90136678868f835fd
lightmen <519542738@qq.com>
2015-06-08 23:21:34 +0800

    modify longest-common-prefix.cpp

--- longest-common-prefix.cpp
+++ longest-common-prefix.cpp
@@ -7,18 +7,16 @@ public:
             return prefix;
         int len0 = strs[0].size();
         int i,j;
-        int k = 0;
 
         for(i = 0; i < len0; ++i){
             for(j = 1; j < len; ++j){
-                if(strs[j][k] != strs[0][k]){
+                if(strs[j][i] != strs[0][i]){
                     break;
                 }
             }
             if(j != len)
                 break;
-            prefix += strs[0][k];
-            k++;
+            prefix += strs[0][i];
         }
 
         return prefix;

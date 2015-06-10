830c3e58b420311c69816f953cda90e01f70d447
lightmen <519542738@qq.com>
2015-06-10 23:22:03 +0800

    add word-break.cpp

---
+++ word-break.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool wordBreak(string s, unordered_set<string>& wordDict) {
+        int len = s.size();
+        bool *flag = new bool[len];
+        memset(flag,0,sizeof(bool) * len);
+
+        for(int i = 0; i < len; ++i){
+            if(wordDict.find(s.substr(0,i+1)) != wordDict.end()){
+                flag[i] = true;
+                continue;
+            }
+            for(int j = 0; j < i; ++j){
+                if(flag[j] &&
+                    wordDict.find(s.substr(j+1,i-j)) != wordDict.end()){
+                    flag[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return flag[len-1];
+    }
+};

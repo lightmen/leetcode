8b2944932ee9f40b1c48d46cf170524c84f9451b
lightmen <519542738@qq.com>
2015-06-20 23:57:54 +0800

    add permutations.cpp

---
+++ permutations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void rec(vector<vector<int>>& vret, vector<int>& nums, vector<int> vt, int *flag, int count)
+    {
+        int size = nums.size();
+        if(count == size){
+            vret.push_back(vt);
+        }else{
+            for(int i = 0; i < size; ++i){
+                if(!flag[i]){
+                    flag[i] = 1;
+                    vt.push_back(nums[i]);
+                    rec(vret,nums,vt,flag,count+1);
+                    vt.pop_back();
+                    flag[i] = 0;
+                }
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int> > vret;
+        vector<int> vt;
+        int len = nums.size();
+        int *flag = new int[len];
+        memset(flag,0,sizeof(int)*len);
+        rec(vret,nums,vt,flag,0);
+
+        return vret;
+    }
+};

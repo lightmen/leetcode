b74af2370cc6f2677fa9388176bafbea03fcde60
lightmen <519542738@qq.com>
2015-06-26 22:26:02 +0800

    add combination-sum-ii.cp

--- combination-sum-ii.cpp
+++ combination-sum-ii.cpp
@@ -3,33 +3,21 @@ public:
     int size;
     int tar;
     vector<vector<int> > vret;
-    bool is_equal(vector<int> &va, vector<int> &vb)
-    {
-        int len = va.size();
-        if(len != vb.size())
-            return false;
-        for(int i = 0; i < len; ++i)
-            if(va[i] != vb[i])
-                return false;
-        return true;
-    }
 
     void rec(vector<int> &vt, vector<int> &cand,int sum, int index)
     {
         if(sum > tar)
             return ;
         if(sum == tar){
-            int i,len;
-            len = vret.size();
-            for(i = 0; i < len; ++i)
-                if(is_equal(vret[i],vt))
-                    break;
-            if(i == len)
-                vret.push_back(vt);
+            vret.push_back(vt);
             return ;
         }
 
+        int prev = -1;
         for(int i = index; i < size; ++i){
+            if(cand[i] == prev)
+                continue;
+            prev = cand[i];
             vt.push_back(cand[i]);
             rec(vt,cand,sum+cand[i],i+1);
             vt.pop_back();

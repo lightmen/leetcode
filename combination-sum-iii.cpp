e6029e7c702fadddc0819250e6573cfbe0550db8
lightmen <519542738@qq.com>
2015-06-26 22:54:41 +0800

    add combination-sum-iii.cp

---
+++ combination-sum-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void helper(vector<vector<int> > &vret, vector<int> &vt, int k, int n, int index)
+    {
+        if(n == 0 && k == 0){
+            vret.push_back(vt);
+            return ;
+        }
+
+        if(index > 9 || n <= 0 || k <= 0)
+            return ;
+
+        for(int i = index; i <= 9; ++i){
+            vt.push_back(i);
+            helper(vret,vt,k-1,n-i,i+1);
+            vt.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int> > vret;
+        vector<int> vt;
+
+        helper(vret,vt,k,n,1);
+
+        return vret;
+    }
+};

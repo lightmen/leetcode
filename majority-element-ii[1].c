4bf5e024dca7bde640c64d7553959b29a5d56b26
lightmen <519542738@qq.com>
2015-07-04 22:37:56 +0800

    modify majority-element-ii[1].c

--- majority-element-ii[1].c
+++ majority-element-ii[1].c
@@ -3,26 +3,36 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* majorityElement(int* nums, int numsSize, int* returnSize) {
-    int *ret = NULL;
+    int *ret;
     int i,c1,c2,arr1, arr2;
 
     arr1 = arr2 = 0;
     c1 = c2 = 0;
     for(i = 0; i < numsSize; ++i){
-        if(arr1 == nums[i])
+        if(arr1 == nums[i]){
             c1++;
-        else if(arr2 == nums[i])
+            continue;
+        }
+
+        if(arr2 == nums[i]){
             c2++;
-        else if(c1 == 0){
+            continue;
+        }
+
+        if(c1 == 0){
             arr1 = nums[i];
             c1++;
-        }else if(c2 == 0){
+            continue;
+        }
+
+        if(c2 == 0){
             arr2 = nums[i];
             c2++;
-        }else{
-            c1--;
-            c2--;
+            continue;
         }
+
+        c1--;
+        c2--;
     }
 
     c1 = c2 = 0;
@@ -33,19 +43,14 @@ int* majorityElement(int* nums, int numsSize, int* returnSize) {
             c2++;
     }
 
-    *returnSize = 0;
-    if(c1 > numsSize / 3 && c2 > numsSize/3){
-        ret = (int *)malloc(sizeof(int) * 2);
-        ret[0] = arr1;
-        ret[1] = arr2;
-        *returnSize = 2;
-    }else if(c1 > numsSize / 3 || c2 > numsSize/3 ){
-        if(c2 > numsSize/3)
-            arr1 = arr2;
-        ret = (int *)malloc(sizeof(int));
-        ret[0] = arr1;
-        *returnSize = 1;
-    }
+    i = 0;
+    ret = (int *)malloc(sizeof(int) * 2);
+    if(c1 > numsSize / 3)
+        ret[i++] = arr1;
 
+    if(c2 > numsSize/3 )
+        ret[i++] = arr2;
+
+    *returnSize = i;
     return ret;
 }

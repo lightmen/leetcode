96e36b533f9decf00b56414981058b2aa7b5739a
lightmen <519542738@qq.com>
2015-07-10 00:50:24 +0800

    modify anagrams.cpp

--- anagrams.cpp
+++ anagrams.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     vector<string> anagrams(vector<string>& strs) {
         vector<string> ret;
-        map<string,string> mp;
-        map<string,string>::iterator mit;
-        string str_eof = "0";
+        map<string,int> mp;
         int len;
+
         len = strs.size();
         for(int i = 0; i < len; ++i){
             string key = strs[i];
             sort(key.begin(),key.end());
 
-            mit = mp.find(key);
-            if( mit == mp.end()){
-                mp.insert(pair<string,string>(key,strs[i]));
+            if( mp.find(key) == mp.end()){
+                mp.insert(pair<string,int>(key,i));
                 continue;
             }
 
-            if(mit->second != str_eof){
-                ret.push_back(mit->second);
-                mp[key] = str_eof;
+            if(mp[key] >= 0){
+                ret.push_back(strs[mp[key]]);
+                mp[key] = -1;
             }
 
             ret.push_back(strs[i]);

2cb4d0c09145f6319c81aa443513bc1e91d33c48
lightmen <519542738@qq.com>
2015-07-13 23:51:23 +0800

    add lowest-common-ancestor-of-a-binary-tree.c

---
+++ lowest-common-ancestor-of-a-binary-tree.c
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    if(!root || root == p || root == q)
+        return root;
+    struct TreeNode *left = lowestCommonAncestor(root->left,p,q);
+    struct TreeNode *right = lowestCommonAncestor(root->right,p,q);
+
+    return !left ? right : (!right ? left : root);
+}

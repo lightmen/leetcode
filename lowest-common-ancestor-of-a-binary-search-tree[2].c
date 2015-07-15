3d8f31d6c108c1d76727f3e3f19fb94070e218ce
lightmen <519542738@qq.com>
2015-07-15 22:53:51 +0800

    modify lowest-common-ancestor-of-a-binary-search-tree[2].c

--- lowest-common-ancestor-of-a-binary-search-tree[2].c
+++ lowest-common-ancestor-of-a-binary-search-tree[2].c
@@ -13,5 +13,8 @@ struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p,
     struct TreeNode *left = lowestCommonAncestor(root->left,p,q);
     struct TreeNode *right = lowestCommonAncestor(root->right,p,q);
 
-    return !left ? right : (!right ? left : root);
+    if(left && right)
+        return root;
+
+    return left ? left : right;
 }

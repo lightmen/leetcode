66d235fb8aab11e70f46064cce9eec3131d907ff
lightmen <519542738@qq.com>
2015-07-16 23:03:27 +0800

    modify lowest-common-ancestor-of-a-binary-tree.c

--- lowest-common-ancestor-of-a-binary-tree.c
+++ lowest-common-ancestor-of-a-binary-tree.c
@@ -9,8 +9,12 @@
 struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
     if(!root || root == p || root == q)
         return root;
+
     struct TreeNode *left = lowestCommonAncestor(root->left,p,q);
     struct TreeNode *right = lowestCommonAncestor(root->right,p,q);
 
-    return !left ? right : (!right ? left : root);
+    if(left && right)
+        return root;
+
+    return left ? left : right;
 }

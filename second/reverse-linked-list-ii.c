7f39393eb33744c2478d45298aa54784f0b03aef
lightmen <519542738@qq.com>
2015-07-24 23:03:02 +0800

    add reverse-linked-list-ii.c

---
+++ second/reverse-linked-list-ii.c
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseBetween(struct ListNode* head, int m, int n) {
+    struct ListNode root;
+    struct ListNode *cur;
+    struct ListNode *tmp;
+    struct ListNode *tail;
+    int i = 1;
+
+    root.next = head;
+    cur = &root;
+
+    while(i < m){
+        i++;
+        cur = cur->next;
+    }
+
+    head = cur->next;
+    tail = head;
+    cur->next = NULL;
+    while(i <= n){
+        i++;
+        tmp = head->next;
+        head->next = cur->next;
+        cur->next = head;
+        head = tmp;
+    }
+
+    if(tail)
+        tail->next = head;
+
+    return root.next;
+}

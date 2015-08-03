6aa8cfa752e5e4908c2ec20e704ad1e7cd650548
lightmen <519542738@qq.com>
2015-08-03 22:42:23 +0800

    add contains-duplicate-ii.c

---
+++ second/array/contains-duplicate-ii.c
@@ -0,0 +1,11 @@
+bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
+    int i,j;
+
+    for(i = 0; i < numsSize; ++i){
+        for(j = i+1; j <= i+k && j < numsSize; ++j)
+            if(nums[j] == nums[i])
+                return true;
+    }
+
+    return false;
+}

5bb453939e9dcbb15f1d533e4c62058906790067
lightmen <519542738@qq.com>
2015-08-04 22:59:39 +0800

    add find-minimum-in-rotated-sorted-array-ii.c

---
+++ second/array/find-minimum-in-rotated-sorted-array-ii.c
@@ -0,0 +1,17 @@
+int findMin(int* nums, int numsSize) {
+    int start,end,mid;
+    start = 0;
+    end = numsSize - 1;
+
+    while(start < end){
+        mid = start + (end - start) / 2;
+        if(nums[mid] > nums[end])
+            start = mid + 1;
+        else if(nums[mid] < nums[end])
+            end = mid;
+        else
+            --end;
+    }
+
+    return nums[start];
+}

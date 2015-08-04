dc99d0f55ea410cae33568a8db9856f995499bf7
lightmen <519542738@qq.com>
2015-08-04 23:21:59 +0800

    modify find-peak-element.c

--- second/array/find-peak-element.c
+++ second/array/find-peak-element.c
@@ -1,8 +1,8 @@
 int findPeakElement(int* nums, int numsSize) {
     int i;
-    for(i = 0; i < numsSize; ++i){
-        if((i == 0 || nums[i] > nums[i-1])
-            && (i == numsSize -1 || nums[i] > nums[i+1]))
-            return i;
-    }
+    for(i = 1; i < numsSize; ++i)
+        if(nums[i-1] > nums[i])
+            return i-1;
+
+    return numsSize - 1;
 }

7ebc7006e57e23248ebb26bffc4740d8074666ec
lightmen <519542738@qq.com>
2015-08-06 00:08:28 +0800

    modify insert-interval.c

--- second/array/insert-interval.c
+++ second/array/insert-interval.c
@@ -9,34 +9,38 @@
  * Return an array of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
  */
-struct Interval* insert(struct Interval* intervals, int intervalsSize, 
+struct Interval* insert(struct Interval* intervals, int intervalsSize,
     struct Interval newInterval, int* returnSize) {
     struct Interval *ret;
     int count = 0;
+    int i;
     int flag = 1;
+
     ret = (struct Interval *)malloc(sizeof(struct Interval) * (intervalsSize + 1));
-    for(int i = 0; i < intervalsSize; ++i){
+
+    for(i = 0; i < intervalsSize; ++i){
         if(intervals[i].end < newInterval.start){
             ret[count].start = intervals[i].start;
             ret[count].end = intervals[i].end;
             count++;
         }else if(intervals[i].start > newInterval.end){
-            if(flag){
-                ret[count].start = newInterval.start;
-                ret[count].end = newInterval.end;
-                count++;
-                flag = 0;
-            }
-
-            ret[count].start = intervals[i].start;
-            ret[count].end = intervals[i].end;
+            ret[count].start = newInterval.start;
+            ret[count].end = newInterval.end;
             count++;
+            flag = 0;
+            break;
         }else{
             newInterval.start = intervals[i].start < newInterval.start ?
                 intervals[i].start : newInterval.start;
             newInterval.end = intervals[i].end > newInterval.end ?
                 intervals[i].end : newInterval.end;
         }
+    }
+
+    for(; i < intervalsSize; ++i){
+        ret[count].start = intervals[i].start;
+        ret[count].end = intervals[i].end;
+        count++;
     }
 
     if(intervalsSize == 0 || flag){

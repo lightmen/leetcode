f2041d31778488986c89081ca8bf1e795e05f4e4
lightmen <519542738@qq.com>
2015-08-06 00:00:38 +0800

    add jump-game.c

---
+++ second/array/jump-game.c
@@ -0,0 +1,16 @@
+bool canJump(int* nums, int numsSize) {
+    int i = 0;
+    int max_jump = i + nums[0];
+
+    while(max_jump < numsSize-1){
+        int max = max_jump;
+        for(; i <= max_jump; ++i)
+            if(i + nums[i] > max)
+                max = i + nums[i];
+        if(max_jump == max)
+            return false;
+        max_jump = max;
+    }
+
+    return true;
+}

7c4e5c61688e7c530b373701a1682a0cd4c38c04
lightmen <519542738@qq.com>
2015-08-10 23:30:48 +0800

    modify maximum-product-subarray.c

--- second/array/maximum-product-subarray.c
+++ second/array/maximum-product-subarray.c
@@ -7,30 +7,27 @@ int maxProduct(int* nums, int numsSize) {
     min = (int *)malloc(sizeof(int) * numsSize);
     max = (int *)malloc(sizeof(int) * numsSize);
 
-    memset(min,0,sizeof(int) * numsSize);
-    memset(max,0,sizeof(int) * numsSize);
 
     min[0] = nums[0];
     max[0] = nums[0];
+    ret = nums[0];
 
     for(i = 1; i < numsSize; ++i){
+        min[i] = max[i] = nums[i];
         if(nums[i] > 0){
-            max[i] = (max[i-1] * nums[i] > nums[i]) ?
-                max[i-1] * nums[i] : nums[i];
-            min[i] = (min[i-1] * nums[i] < nums[i]) ?
-                min[i-1] * nums[i] : nums[i];
+            max[i] = (max[i-1] * nums[i] > max[i]) ?
+                max[i-1] * nums[i] : max[i];
+            min[i] = (min[i-1] * nums[i] < min[i]) ?
+                min[i-1] * nums[i] : min[i];
         }else if(nums[i] < 0){
-            max[i] = (min[i-1] * nums[i] > nums[i]) ?
-                min[i-1] * nums[i] : nums[i];
-            min[i] = (max[i-1] * nums[i] < nums[i]) ?
-                max[i-1] * nums[i] : nums[i];
+            max[i] = (min[i-1] * nums[i] > max[i]) ?
+                min[i-1] * nums[i] : max[i];
+            min[i] = (max[i-1] * nums[i] < min[i]) ?
+                max[i-1] * nums[i] : min[i];
         }
-    }
 
-    ret = max[0];
-    for(i = 1; i < numsSize; ++i)
-        if(max[i] > ret)
-            ret = max[i];
+        ret = max[i] > ret ? max[i] : ret;
+    }
 
     free(min);
     free(max);

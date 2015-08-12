eb34e2a48f77a1205b2ab4dd76c6a67afe511b72
lightmen <519542738@qq.com>
2015-08-12 22:40:38 +0800

    add median-of-two-sorted-arrays.c

---
+++ second/array/median-of-two-sorted-arrays.c
@@ -0,0 +1,26 @@
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    int *arr;
+    int num = nums1Size + nums2Size;
+    int i,j,k;
+    double ret;
+
+    arr = (int *)malloc(sizeof(int) * num);
+    i = 0;
+    j = 0;
+    k = 0;
+    while(i < nums1Size && j < nums2Size)
+        arr[k++] = nums1[i] < nums2[j] ? nums1[i++] : nums2[j++];
+
+    while(i < nums1Size)
+        arr[k++] = nums1[i++];
+    while(j < nums2Size)
+        arr[k++] = nums2[j++];
+
+    if(num % 2)
+        ret = arr[num/2];
+    else
+        ret = (double)(arr[(num-1)/2] + arr[num/2]) / 2;
+
+    free(arr);
+    return ret;
+}

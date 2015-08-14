31bfce005ed69e8e586e2d92c8b15f264c45a821
lightmen <519542738@qq.com>
2015-08-14 22:40:01 +0800

    add plus-one.c

---
+++ second/array/plus-one.c
@@ -0,0 +1,27 @@
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    int *arr;
+    int i;
+    int carry = 1;
+    int tmp;
+    int offset = 1;
+
+    arr = (int *)malloc(sizeof(int) * (digitsSize+1));
+
+    for(i = digitsSize-1; i >= 0; --i){
+        tmp = digits[i] + carry;
+        carry = tmp / 10;
+        arr[i+1] = tmp % 10;
+    }
+
+    if(carry){
+        arr[0] = carry;
+        offset--;
+    }
+
+    *returnSize = digitsSize + 1 - offset;
+    return arr + offset;
+}

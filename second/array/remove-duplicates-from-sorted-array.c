8ff030dfdf8d05ed30c43311ce847adb29465497
lightmen <519542738@qq.com>
2015-08-15 21:49:46 +0800

    add remove-duplicates-from-sorted-array.c

--- second/array/remove-duplicates-from-sorted-array.c
+++ second/array/remove-duplicates-from-sorted-array.c
@@ -1,14 +1,13 @@
 int removeDuplicates(int* nums, int numsSize) {
-    int count,j;
+    int count,i;
 
     if(numsSize == 0)
         return 0;
 
     count = 1;
-    for(j = 1; j < numsSize; ++j){
-        if(nums[j] == nums[j-1])
-            continue;
-        nums[count++] = nums[j];
+    for(i = 1; i < numsSize; ++i){
+        if(nums[i] != nums[count-1])
+            nums[count++] = nums[i];
     }
 
     return count;

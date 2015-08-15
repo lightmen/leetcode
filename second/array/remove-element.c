cda1860d64e070f96010a841f79da42cea202e95
lightmen <519542738@qq.com>
2015-08-15 22:08:38 +0800

    modify remove-element.c

--- second/array/remove-element.c
+++ second/array/remove-element.c
@@ -1,12 +1,11 @@
 int removeElement(int* nums, int numsSize, int val) {
-    int ret = 0;
-    int cur = 0;
+    int count,i;
 
-    while(cur < numsSize){
-        if(nums[cur] != val)
-            nums[ret++] = nums[cur];
-        cur++;
+    count = 0;
+    for(i = 0; i < numsSize; ++i){
+        if(nums[i] != val)
+            nums[count++] = nums[i];
     }
 
-    return ret;
+    return count;
 }

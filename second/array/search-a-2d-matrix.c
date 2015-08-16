e0b81e4ea2fc38ca4aeb2dc2c3f67ce8c6df60fa
lightmen <519542738@qq.com>
2015-08-16 11:51:42 +0800

    modify search-a-2d-matrix.c

---
+++ second/array/search-a-2d-matrix.c
@@ -0,0 +1,21 @@
+bool searchMatrix(int** matrix, int matrixRowSize, int matrixColSize, int target) {
+    int start,end;
+    int mid;
+    int tmp;
+
+    start = 0;
+    end = matrixRowSize * matrixColSize - 1;
+
+    while(start <= end){
+        mid = (start + end) / 2;
+        tmp = matrix[mid / matrixColSize][mid % matrixColSize];
+        if(tmp == target)
+            return true;
+        else if(tmp > target)
+            end = mid - 1;
+        else
+            start = mid + 1;
+    }
+
+    return false;
+}

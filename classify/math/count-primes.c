cff87adcea8b452042ce8142fb64dcc4f8cd666a
lightmen <519542738@qq.com>
2015-09-10 23:59:53 +0800

    add count-primes.c

---
+++ classify/math/count-primes.c
@@ -0,0 +1,19 @@
+int countPrimes(int n) {
+    int ret = 0;
+    int i,j;
+    int *prime;
+
+    prime = (int *)malloc(sizeof(int) * n);
+    memset(prime,0,sizeof(int) * n);
+
+    for(i = 2; i < n; ++i){
+        if(prime[i])
+            continue;
+        ret++;
+        for(j = 2; i * j < n; ++j)
+            prime[i*j] = 1;
+    }
+
+    free(prime);
+    return ret;
+}

eecec597c56ff9d800367a5fce08d1a3a794ef2f
lightmen <519542738@qq.com>
2015-09-18 00:51:51 +0800

    add integer-to-roman.c

---
+++ classify/math/integer-to-roman.c
@@ -0,0 +1,33 @@
+char* intToRoman(int num) {
+    char symbols[][3] = {
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I",
+    };
+
+    int values[] = {
+        1000, 900, 500, 400,
+        100,  90,  50,  40,
+        10,   9,   5,   4,
+        1,
+    };
+
+    char *ret;
+    int i,j;
+
+    i = 0;
+    ret = (char *)malloc(sizeof(char) * 32);
+    memset(ret,0,sizeof(char) * 32);
+
+    while(num > 0){
+        int k = num / values[i];
+        for(j = 0; j < k; ++j){
+            strcat(ret,symbols[i]);
+            num -= values[i];
+        }
+        i++;
+    }
+
+    return ret;
+}

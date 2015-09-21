8de8edf29a983dd6681698406fd207a98b372244
lightmen <519542738@qq.com>
2015-09-22 01:00:35 +0800

    modify palindrome-number.c

--- classify/math/palindrome-number.c
+++ classify/math/palindrome-number.c
@@ -1,19 +1,13 @@
 bool isPalindrome(int x) {
     if(x < 0)
         return false;
-    long rx = 0;
+
+    int rx = 0;
     int tmp = x;
     while(tmp){
         rx = 10 * rx + (tmp % 10);
         tmp /= 10;
     }
 
-    while(x){
-        if(rx % 10 != x % 10)
-            return false;
-        rx /= 10;
-        x /= 10;
-    }
-
-    return rx == 0 ? true : false;
+    return rx == x;
 }

f92d6a0e8135e37e8e593eab774c8b26056cb6ad
lightmen <519542738@qq.com>
2015-09-23 22:53:28 +0800

    modify reverse-integer.c

--- classify/math/reverse-integer.c
+++ classify/math/reverse-integer.c
@@ -1,21 +1,15 @@
 int reverse(int x) {
-    long rx = 0;
-    long lx = x;
-    int is_neg = 0;
-
-    if(lx < 0){
-        lx = -lx;
-        is_neg = 1;
+    int rx = 0;
+    int ret = 0;
+    while(x){
+        ret = rx * 10 + x % 10;
+        if(ret / 10 != rx){
+            ret = 0;
+            break;
+        }
+        rx = ret;
+        x /= 10;
     }
 
-    while(lx){
-        rx = 10 * rx + lx % 10;
-        lx /= 10;
-    }
-
-    rx = is_neg ? -rx : rx;
-    if(rx > INT_MAX || rx < INT_MIN)
-        return 0;
-
-    return rx;
+    return ret;
 }

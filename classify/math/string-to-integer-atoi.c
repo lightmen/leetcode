154110387ee855d0663ef852a8e830e1522dd0be
lightmen <519542738@qq.com>
2015-09-25 22:21:33 +0800

    add string-to-integer-atoi.c

---
+++ classify/math/string-to-integer-atoi.c
@@ -0,0 +1,24 @@
+int myAtoi(char* str) {
+    int ret = 0;
+    int tmp = 0;
+    int is_neg = 0;
+
+    while(*str && *str == ' ')
+        str++;
+
+    if(*str == '-')
+        is_neg = 1;
+
+    if(*str == '+' || *str == '-')
+        str++;
+
+    while(*str >= '0' && *str <= '9'){
+        tmp = ret * 10 + (*str - '0');
+        str++;
+        if(tmp / 10 != ret)
+            return is_neg ? INT_MIN : INT_MAX;
+        ret = tmp;
+    }
+
+    return is_neg ? -ret : ret;
+}

443481f547c88611ece4d75efc348a28ee303610
lightmen <519542738@qq.com>
2015-12-07 00:01:35 +0800

    add kth-smallest-element-in-a-bst[1].c

---
+++ classify/binary-search/kth-smallest-element-in-a-bst[1].c
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int getKth(struct TreeNode *root, int k, int *val)
+{
+    if(root == NULL)
+        return 0;
+
+    int left = getKth(root->left,k,val);
+    if(left + 1 > k)
+        return left;
+    if(left + 1 == k){
+        *val = root->val;
+        return left+1;
+    }
+
+    return left + 1 + getKth(root->right,k-left-1,val);
+}
+
+int kthSmallest(struct TreeNode* root, int k) {
+    int val;
+    getKth(root,k, &val);
+
+    return val;
+}

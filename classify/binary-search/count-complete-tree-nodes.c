748041c0c168a8c46c12f02c3af2269fc779b277
lightmen <519542738@qq.com>
2015-12-14 23:14:09 +0800

    add count-complete-tree-nodes.c

---
+++ classify/binary-search/count-complete-tree-nodes.c
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int countNodes(struct TreeNode* root) {
+    if(root == NULL)
+        return 0;
+
+    int lh,rh;
+    struct TreeNode *tmp;
+
+    tmp = root->left;
+    lh = 0;
+    while(tmp){
+        lh++;
+        tmp = tmp->left;
+    }
+
+    tmp = root->right;
+    rh = 0;
+    while(tmp){
+        rh++;
+        tmp = tmp->right;
+    }
+
+    if(lh == rh)
+        return pow(2,lh+1) - 1;
+
+    return 1 + countNodes(root->left) + countNodes(root->right);
+}

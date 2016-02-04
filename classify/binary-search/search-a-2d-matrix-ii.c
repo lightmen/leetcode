20dbc350cd401d03dc0ed37ddbdbc0495ce48acf
lightmen <519542738@qq.com>
2016-02-05 00:17:27 +0800

    add search-a-2d-matrix-ii.c

---
+++ classify/binary-search/search-a-2d-matrix-ii.c
@@ -0,0 +1,16 @@
+bool searchMatrix(int** matrix, int matrixRowSize, int matrixColSize, int target) {
+    int row,col;
+
+    row = matrixRowSize - 1;
+    col = 0;
+    while(row >= 0 && col < matrixColSize){
+        if(matrix[row][col] == target)
+            return true;
+        if(matrix[row][col] > target)
+            row--;
+        else
+            col++;
+    }
+
+    return false;
+}

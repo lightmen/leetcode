02a679ee51324b980ddf1deb99a102e9c700cbac
lightmen <519542738@qq.com>
2016-02-15 23:24:03 +0800

    add kth-largest-element-in-an-array.c

---
+++ classify/divide-and-conquer/kth-largest-element-in-an-array.c
@@ -0,0 +1,10 @@
+int cmp(const void *a, const void *b)
+{
+    return *(int *)b - *(int *)a;
+}
+
+int findKthLargest(int* nums, int numsSize, int k) {
+    qsort(nums,numsSize,sizeof(int),cmp);
+
+    return nums[k-1];
+}

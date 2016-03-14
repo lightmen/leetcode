d2571a77c67178725eeb89133a5dfa3541da05e3
lightmen <519542738@qq.com>
2016-03-15 00:39:32 +0800

    add maximum-product-subarray.c

---
+++ classify/dynamic-programming/maximum-product-subarray.c
@@ -0,0 +1,38 @@
+#define max_2(x, y) ({ \
+    int _x = (x); \
+    int _y = (y); \
+    _x > _y ? _x : _y; \
+})
+
+#define min_2(x, y) ({ \
+    int _x = (x); \
+    int _y = (y); \
+    _x < _y ? _x : _y; \
+})
+
+int maxProduct(int* nums, int numsSize) {
+    int cur_min, cur_max;
+    int prev_min, prev_max;
+    int max_value;
+    int i;
+
+    prev_min = nums[0];
+    prev_max = nums[0];
+    max_value = nums[0];
+    for(i = 1; i < numsSize; ++i){
+        cur_min = nums[i];
+        cur_max = nums[i];
+        if(nums[i] > 0){
+            cur_max = max_2(cur_max, prev_max * nums[i]);
+            cur_min = min_2(cur_min, prev_min * nums[i]);
+        }else{
+            cur_max = max_2(cur_max, prev_min * nums[i]);
+            cur_min = min_2(cur_min, prev_max * nums[i]);
+        }
+        max_value = max_2(max_value, cur_max);
+        prev_min = cur_min;
+        prev_max = cur_max;
+    }
+
+    return max_value;
+}

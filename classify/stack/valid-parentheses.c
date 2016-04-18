1342eb89c0deac9cb3a62a25438c8e90c8b2cb4e
lightmen <519542738@qq.com>
2016-04-18 22:30:40 +0800

    add valid-parentheses.c

---
+++ classify/stack/valid-parentheses.c
@@ -0,0 +1,29 @@
+bool is_match(char left, char right)
+{
+    return (left == '(' && right == ')') ||
+        (left == '{' && right == '}') ||
+        (left == '[' && right == ']');
+}
+
+bool isValid(char* s) {
+    int *st;
+    int top;
+    int len;
+
+    len = strlen(s);
+    st = malloc(sizeof(int) * len);
+    top = -1;
+
+    while(*s){
+        if(*s == '(' || *s == '[' || *s == '{'){
+            st[++top] = *s;
+        }else if(top > -1 && is_match(st[top], *s)){
+            top--;
+        }else{
+            return false;
+        }
+        s++;
+    }
+
+    return top == -1;
+}

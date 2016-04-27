0037ba36cbfd481d29ca688b9280cf48da777f70
lightmen <519542738@qq.com>
2016-04-27 23:33:50 +0800

    add binary-tree-postorder-traversal[2].py

---
+++ python/stack/binary-tree-postorder-traversal[2].py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        st = []
+        data = []
+        pre = None
+
+        if root is not None:
+            st.append(root)
+            while st:
+                cur = st[len(st)-1]
+                if ((cur.left == None and cur.right == None) or \
+                    (pre  and (pre == cur.left or pre == cur.right))) :
+                    data.append(cur.val)
+                    st.pop()
+                    pre = cur
+                else :
+                    if cur.right:
+                        st.append(cur.right)
+                    if cur.left:
+                        st.append(cur.left)
+
+        return data

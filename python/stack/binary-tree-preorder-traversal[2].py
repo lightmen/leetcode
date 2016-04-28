4f19be33175b177c45b43e076a1c127c2e356142
lightmen <519542738@qq.com>
2016-04-28 23:06:31 +0800

    add binary-tree-preorder-traversal[2].py

---
+++ python/stack/binary-tree-preorder-traversal[2].py
@@ -0,0 +1,13 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        if root:
+            return [root.val] + self.preorderTraversal(root.left) + \
+                self.preorderTraversal(root.right)
+        return []

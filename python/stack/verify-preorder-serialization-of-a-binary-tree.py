f13a59ec9827a12fbccb270e8ab35990c67145b3
lightmen <519542738@qq.com>
2016-05-21 23:14:31 +0800

    modify verify-preorder-serialization-of-a-binary-tree.py

--- python/stack/verify-preorder-serialization-of-a-binary-tree.py
+++ python/stack/verify-preorder-serialization-of-a-binary-tree.py
@@ -1,21 +1,20 @@
 class Solution(object):
     def isValidSerialization(self, preorder):
-        st = []
+        count = 0
         s = preorder.split(',')
         slen = len(s)
         if s[0] != '#':
-            st.append(s[0])
+            count += 1
 
         i = 1;
-        while st:
+        while count:
             if i == slen or i + 1 == slen:
                 return False
-            st.pop();
-            if s[i+1] != '#':
-                st.append(s[i+1])
-
+            count -= 1
             if s[i] != '#':
-                st.append(s[i])
+                count += 1
+            if s[i+1] != '#':
+                count += 1
             i += 2
 
         if i < slen:

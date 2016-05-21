0c47e6f0d969eaa82fbbe99fecd319dc8c581586
lightmen <519542738@qq.com>
2016-05-22 00:03:53 +0800

    addverify-preorder-serialization-of-a-binary-tree.c

---
+++ c/stack/verify-preorder-serialization-of-a-binary-tree.c
@@ -0,0 +1,39 @@
+bool isValidSerialization(char* preorder) {
+    int count = 0;
+    char *next = preorder;
+    char *left,*right;
+
+    if(*next && *next != '#')
+        count++;
+    while(*next && *next != ',')
+            next++;
+    while (count-- > 0){
+
+        if(*next)
+            next++;
+        left = next;
+        if(!*left)
+            return false;
+
+        while(*next && *next != ',')
+            next++;
+        if(*next)
+            next++;
+        right = next;
+        if(!*right)
+            return false;
+
+        if(*left != '#')
+            count++;
+        if(*right != '#')
+            count++;
+
+        while(*next && *next != ',')
+            next++;
+    }
+
+    if(*next)
+        return false;
+
+    return true;
+}

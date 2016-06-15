06d1ec21f4280b47384da95b4015a7b4136af30e
lightmen <519542738@qq.com>
2016-06-15 23:51:35 +0800

    add binary-tree-paths.py

---
+++ python/tree/binary-tree-paths.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @return {string[]}
+    def __init__(self):
+        self.path = []
+
+    def rescuive(self,root,cur_path):
+        if not root:
+            return
+
+        cur_path += str(root.val)
+        if root.left:
+            self.rescuive(root.left,cur_path + '->')
+        if root.right:
+            self.rescuive(root.right,cur_path + '->')
+
+        if not root.left and not root.right:
+            self.path.append(cur_path)
+
+    def binaryTreePaths(self, root):
+        self.rescuive(root,'')
+        return self.path

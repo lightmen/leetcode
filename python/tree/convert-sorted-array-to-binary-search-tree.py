9ef3b2739821e76a066edcb4890be442274d9683
lightmen <519542738@qq.com>
2016-06-21 23:17:15 +0800

    modify convert-sorted-array-to-binary-search-tree.py

--- python/tree/convert-sorted-array-to-binary-search-tree.py
+++ python/tree/convert-sorted-array-to-binary-search-tree.py
@@ -6,17 +6,17 @@
 #         self.right = None
 
 class Solution(object):
-    def bsearch(self,start,end):
+    def bcreate(self,start,end):
         if end < start:
             return None
 
         mid = (start + end) / 2
         node = TreeNode(self.nums[mid])
-        node.left = self.bsearch(start,mid -1)
-        node.right = self.bsearch(mid + 1, end)
+        node.left = self.bcreate(start,mid -1)
+        node.right = self.bcreate(mid + 1, end)
 
         return node
 
     def sortedArrayToBST(self, nums):
         self.nums = nums
-        return self.bsearch(0,len(self.nums) - 1)
+        return self.bcreate(0,len(self.nums) - 1)

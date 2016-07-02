f8549549994b583edba8e3961efb637d4317dcf4
lightmen <519542738@qq.com>
2016-07-02 20:07:15 +0800

    modify path-sum-ii.py

--- python/tree/path-sum-ii.py
+++ python/tree/path-sum-ii.py
@@ -6,21 +6,21 @@
 #         self.right = None
 
 class Solution(object):
+    def dfs(self,root,sum,cur_arr):
+        if root.left is None and root.right is None:
+            if root.val == sum:
+                self.result.append(cur_arr)
+        else:
+            if root.left:
+                self.dfs(root.left, sum - root.val,cur_arr + [root.left.val])
+            if root.right:
+                self.dfs(root.right, sum - root.val,cur_arr + [root.right.val])
 
 
     def pathSum(self, root, sum):
-        def dfs(root,sum,cur_arr):
-            if root.left is None and root.right is None:
-                if root.val == sum:
-                    result.append(cur_arr)
-            else:
-                if root.left:
-                    dfs(root.left, sum - root.val,cur_arr + [root.left.val])
-                if root.right:
-                    dfs(root.right, sum - root.val,cur_arr + [root.right.val])
+        self.result = []
 
-        result = []
         if root:
-            dfs(root,sum,[root.val])
+            self.dfs(root,sum,[root.val])
 
-        return result
+        return self.result

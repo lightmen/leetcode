7a1e3709ecf321f6fe84639fe323ea1ba2b43227
lightmen <519542738@qq.com>
2016-08-02 18:05:25 +0800

    modify binary-tree-inorder-traversal.py

--- python/hash-table/binary-tree-inorder-traversal.py
+++ python/hash-table/binary-tree-inorder-traversal.py
@@ -6,14 +6,9 @@
 #         self.right = None
 
 class Solution(object):
-    def __init__(self):
-        self.res = []
-
     def inorderTraversal(self, root):
         if root is None:
             return []
 
-        self.inorderTraversal(root.left)
-        self.res.append(root.val)
-        self.inorderTraversal(root.right)
-        return self.res
+        return self.inorderTraversal(root.left) + [root.val] \
+            + self.inorderTraversal(root.right)

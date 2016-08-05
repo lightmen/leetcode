67ffd29427d71eb6d51cc0e5b87a803ab3548dd8
lightmen <519542738@qq.com>
2016-08-05 21:16:25 +0800

    add contains-duplicate.py

--- python/hash-table/contains-duplicate.py
+++ python/hash-table/contains-duplicate.py
@@ -1,10 +1,8 @@
 class Solution(object):
     def containsDuplicate(self, nums):
         nums.sort()
-        length = len(nums)
-
-        for i in range(1,length):
-            if nums[i] == nums[i-1]:
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
                 return True
 
         return False

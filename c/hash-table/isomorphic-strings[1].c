c71e52cf73b951ee4373aac2a03df9620e5e3cd8
lightmen <519542738@qq.com>
2016-08-19 21:36:27 +0800

    modify isomorphic-strings[1].c

--- c/hash-table/isomorphic-strings[1].c
+++ c/hash-table/isomorphic-strings[1].c
@@ -5,12 +5,11 @@ bool isIsomorphic(char* s, char* t) {
     int len = strlen(s);
 
     for(int i = 0; i < len; ++i){
-        if((ms[s[i]] && ms[s[i]] !=  t[i]) ||
-            (mt[t[i]] && mt[t[i]] != s[i]))
+        if(ms[s[i]] != mt[t[i]])
             return false;
 
-        ms[s[i]] = t[i];
-        mt[t[i]] = s[i];
+        ms[s[i]] = i + 1;
+        mt[t[i]] = i + 1;
     }
 
     return true;

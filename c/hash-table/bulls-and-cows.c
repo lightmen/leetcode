ec900ad306126475093caef42c042b4a40434fb0
lightmen <519542738@qq.com>
2016-08-21 18:00:49 +0800

    modify bulls-and-cows.c

--- c/hash-table/bulls-and-cows.c
+++ c/hash-table/bulls-and-cows.c
@@ -4,25 +4,19 @@ char* getHint(char* secret, char* guess) {
     int b = 0;
     int len = strlen(guess);
     int m[256] = {0};
-    int *visited = malloc(sizeof(int)*len);
 
     ret = malloc(sizeof(int) * (len+2));
-    memset(visited, 0, sizeof(int)*len);
 
     for(int i = 0; i < len; ++i){
         if(secret[i] == guess[i]){
             ++a;
-            visited[i] = 1;
         }else{
             m[secret[i]]++;
         }
     }
 
     for(int i = 0; i < len; ++i){
-        if(visited[i])
-            continue;
-
-        if(m[guess[i]]){
+        if(secret[i] != guess[i] && m[guess[i]]){
             m[guess[i]]--;
             ++b;
         }
@@ -31,6 +25,5 @@ char* getHint(char* secret, char* guess) {
     len = sprintf(ret, "%dA%dB", a, b);
     ret[len] = 0;
 
-    free(visited);
     return ret;
 }

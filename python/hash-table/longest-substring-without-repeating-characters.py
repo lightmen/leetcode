5bc49b111be80f45f9203fb4a2082d0ffb7c80ea
lightmen <519542738@qq.com>
2016-08-30 21:07:28 +0800

    modify longest-substring-without-repeating-characters.py

--- python/hash-table/longest-substring-without-repeating-characters.py
+++ python/hash-table/longest-substring-without-repeating-characters.py
@@ -1,14 +1,12 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         index = {}
-        last_index = -1
+        last_index = 0
         ret = 0
         for i in xrange(len(s)):
-            index[s[i]] = -1
-
-        for i in xrange(len(s)):
-            last_index = index[s[i]] + 1 if last_index <= index[s[i]] else last_index
+            if index.get(s[i],-1) >= last_index:
+                last_index = index[s[i]] + 1
             index[s[i]] = i
-            ret = (i - last_index + 1) if (i - last_index + 1) > ret else ret
+            ret = max((i - last_index + 1),ret)
 
         return ret

df2c85cb69a11cd2dee4f255f9ca63aab373629f
lightmen <519542738@qq.com>
2016-09-29 21:57:59 +0800

    modify intersection-of-two-arrays.py

--- python/hash-table/intersection-of-two-arrays.py
+++ python/hash-table/intersection-of-two-arrays.py
@@ -1,3 +1,4 @@
 class Solution(object):
     def intersection(self, nums1, nums2):
         return [val for val in set(nums1) if val in nums2]
+        #'return list(set(nums1) & set(nums2))' is also OK

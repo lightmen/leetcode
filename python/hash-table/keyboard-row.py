64d14f0150ac3fac5ac31421f18787fdbe1e2403
lightmen <519542738@qq.com>
2017-02-21 22:34:56 +0800

    modify keyboard-row.py

--- python/hash-table/keyboard-row.py
+++ python/hash-table/keyboard-row.py
@@ -4,17 +4,16 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[str]
         """
-        line = ["qwertyuiopQWERTYUIOP", "asdfghjklASDFGHJKL", \
-            "zxcvbnmZXCVBNM"]
+        line = ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
         res = []
         for word in words:
             for m in line:
-                if word[0] in m:
+                if word[0].lower() in m:
                     l = m
                     break
             flag = 1
             for w in word:
-                if w not in l:
+                if w.lower() not in l:
                     flag = 0
                     break
             if flag:

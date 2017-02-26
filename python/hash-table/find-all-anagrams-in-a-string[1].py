e49660e936ab336252f8b95308e9707c0d1ee427
lightmen <519542738@qq.com>
2017-02-26 21:35:02 +0800

    add find-all-anagrams-in-a-string[1].py

---
+++ python/hash-table/find-all-anagrams-in-a-string[1].py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        ls, lp = len(s), len(p)
+        cp = collections.Counter(p)
+        ret = []
+        count = lp
+
+        for i in range(ls):
+            if cp[s[i]] > 0:
+                count -= 1
+            cp[s[i]] -= 1
+            if i >= lp:
+                if cp[s[i - lp]] >= 0:
+                    count += 1
+                cp[s[i - lp]] += 1
+
+            if count == 0:
+                ret.append(i - lp + 1)
+
+        return ret

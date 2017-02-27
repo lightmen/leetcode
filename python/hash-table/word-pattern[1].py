ee9652a18f8e284e32b2b926887fb17301b8dc5e
lightmen <519542738@qq.com>
2017-02-27 23:32:09 +0800

    add word-pattern[1].py

---
+++ python/hash-table/word-pattern[1].py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        words = str.split()
+        return map(pattern.find, pattern) == map(words.index, words)

373b66a7c9e8d6f366ab38b1e952e63f3755400f
lightmen <zero_he@foxmail.com>
2017-06-27 10:10:26 +0800

    add remove-duplicates-from-sorted-array.go

---
+++ go/array/remove-duplicates-from-sorted-array.go
@@ -0,0 +1,14 @@
+func removeDuplicates(nums []int) int {
+    i, j := 0, 0
+    for j < len(nums) {
+        for j < len(nums) && nums[i] == nums[j] {
+            j++
+        }
+        if j < len(nums) {
+            i++
+            nums[i] = nums[j]
+        }
+    }
+
+    return i + 1
+}

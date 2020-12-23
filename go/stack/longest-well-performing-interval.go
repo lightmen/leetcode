86f6a1c55e196a433c18fcca78c6726f04104b2d
lightmen <zero_he@foxmail.com>
2020-12-23 21:50:25 +0800

    optimize longest-well-performing-interval

--- go/stack/longest-well-performing-interval.go
+++ go/stack/longest-well-performing-interval.go
@@ -3,6 +3,8 @@ package stack
 func longestWPI(hours []int) int {
 	preSum :=[]int{0}
 	sum := 0
+
+
 	for _, h := range hours {
 		if h > 8 {
 			sum++
@@ -11,13 +13,23 @@ func longestWPI(hours []int) int {
 		}
 
 		preSum = append(preSum, sum)
+
+
+	}
+
+	st := make([]int, 0)
+	for idx, val := range preSum {
+		if len(st) == 0 || preSum[st[len(st)-1]] > val {
+			st = append(st, idx)
+		}
 	}
 
 	maxWPI := 0
-	for i := 0; i <len(preSum); i++ {
-		for j := len(preSum)-1; j > i; j-- {
-			if preSum[j] >preSum[i] && j -i  >maxWPI {
-				maxWPI = j - i
+	for i := 0; i <len(st); i++ {
+		idx := st[i]
+		for j := len(preSum)-1; j > idx; j-- {
+			if preSum[j] >preSum[idx] && j - idx  >maxWPI {
+				maxWPI = j - idx
 				break
 			}
 		}

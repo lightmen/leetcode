6d222145ba26832a89effd2e5b44a2f7ef391c2a
lightmen <zero_he@foxmail.com>
2020-12-30 22:06:14 +0800

    optimize 132-pattern

--- go/stack/132-pattern.go
+++ go/stack/132-pattern.go
@@ -1,18 +1,37 @@
 package stack
 
 func find132pattern(nums []int) bool {
-	for i := 2; i < len(nums); i++ {
-		status := 0
-		for j := i -1; j >= 0; j-- {
-			if status == 0 && nums[j] > nums[i] {
-				status = 1
-				continue
-			}
+	if len(nums) < 3 {
+		return false
+	}
 
-			if status == 1 && nums[j] < nums[i] {
-				return true
-			}
+	min := make([]int, len(nums))
+	min[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		min[i] = min[i-1]
+		if nums[i] < min[i] {
+			min[i] = nums[i]
 		}
+	}
+
+	st := make([]int, len(nums))
+	top := -1
+
+	for j := len(nums) -1; j >= 0; j-- {
+		if nums[j] <= min[j] {
+			continue
+		}
+
+		for top > -1 && min[j] >= st[top] {
+			top--
+		}
+
+		if top > -1 && nums[j] > st[top] {
+			return true
+		}
+
+		top++
+		st[top] = nums[j]
 	}
 
 	return false
